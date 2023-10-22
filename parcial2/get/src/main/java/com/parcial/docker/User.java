23e338ad7d616c88a52f3c9e6853eb0e860da54d
Z4ND3RX <andresmauricioocampo2@gmail.com>
2023-10-22 15:33:56 -0500

    Parcial2 edited with Crud of the spring project of part 2 separated by microservices Add, Put, Get, Delete - and updated docker-compose.yml file

---
+++ parcial2/get/src/main/java/com/parcial/docker/User.java
@@ -0,0 +1,64 @@
+package com.parcial.docker;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @Column(name="name")
+    private String name;
+
+    @Column(name="last_name")
+    private String last_name;
+
+    @Column(name="gender")
+    private String gender;
+
+    @Column(name="age")
+    private int age;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLast_name() {
+        return last_name;
+    }
+
+    public void setLast_name(String last_name) {
+        this.last_name = last_name;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+}
+
